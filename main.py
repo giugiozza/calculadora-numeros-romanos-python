0dce7c277ec78ada0f1c96651ba59c535a226e26
Giulia Giozza <giuliagiozza@gmail.com>
2020-07-17 23:33:24 -0300

    leitura das linhas inicializada
    sem erro, mas falta testar

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+linhas = []
+dicionario_de_unidades = {}
+dicionario_de_creditos = {}
+
 print("Seja bem-vind@, Vendedor Interespacial!\n")
 
 nome_arquivo = input("Por favor, digite o nome do arquivo (sem a extensão): ")
@@ -9,7 +13,35 @@ try:
 except:
     print("Arquivo não encontrado.")
 
+linhas = arquivo.readlines()
+for linha in linhas:
+    linha = linha.rstrip()
+    if not(linha.endswith("?")):
+        if not(linha.endswith("créditos")):
+            array_da_linha = linha.partition("representa")
+            dicionario_de_unidades[array_da_linha[0].strip()] = array_da_linha[2].strip()
+        elif (linha.endswith("créditos")):
+            linha.replace('créditos', '')
+            array_da_linha = linha.partition("valem")
+            dicionario_de_creditos[array_da_linha[0].strip()] = array_da_linha[2].strip()
+    elif (linha.startswith("quanto vale")):
+        linha.replace('?', '')
+        array_da_linha = linha.partition("são")
+        #TODO
+        #<função calcula valor> = array_da_linha[2].strip()
+    elif (linha.startswith("quantos créditos")):
+        linha.replace('?', '')
+        array_da_linha = linha.partition("são")
+        #TODO
+        #<função calcula credito> = array_da_linha[2].strip()
+'''
+print(len(linhas))
+print("\n" + linhas[-1]+"\n")
+print(linhas[-1].endswith("?"))
+'''
 
+
+# FINAL #
 
 try:
     arquivo.close()
